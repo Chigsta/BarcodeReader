2fa1477304ac5f14ccf41e20a9f310bcbd677df1
Chiraag Patel <chiraag.patel@hotmail.co.uk>
2020-03-24 07:15:31 +0000

    Initial Commit

---
+++ BarcodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeReader
+//
+//  Created by Chiraag Patel on 24/03/2020.
+//  Copyright © 2020 Chiraag Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

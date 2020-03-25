3b04d354392e7b894986beaad77b01a724a5e41e
Chiraag Patel <chiraag.patel@hotmail.co.uk>
2020-03-25 07:52:44 +0000

    refactor code to Mark: Select Photo

--- BarcodeReader/ViewController.swift
+++ BarcodeReader/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,45 +20,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
+}
+
+// MARK: - Select Photo
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @objc func promptPhoto() {
-        
-        // Create an Alert prompting user to select a option to pick an image
-        let prompt = UIAlertController(title: "Choose A Photo", message: "Please choose an option", preferredStyle: .alert)
-       
-        // Functions to provide in the handler for addAction to allow camera, photo album, photo libray usuage
-        
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        func presentCamera(_ _ : UIAlertAction) {
-            imagePicker.sourceType = .camera
-            present(imagePicker, animated: true, completion: nil)
-        }
-        
-        func presentPhotoLibrary(_ _ : UIAlertAction) {
-                   imagePicker.sourceType = .photoLibrary
-                   present(imagePicker, animated: true, completion: nil)
-               }
-        
-        func presentPhotoAlbum(_ _ : UIAlertAction) {
-                          imagePicker.sourceType = .savedPhotosAlbum
-                          present(imagePicker, animated: true, completion: nil)
-                      }
-        
-        // Actions to add to the promt alert
-        
-        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: presentCamera)
-        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: presentPhotoLibrary)
-        let photoAlbumAction = UIAlertAction(title: "Photo Album", style: .default, handler: presentPhotoAlbum)
-        
-        // Add actions with Function handler to the prompt
-        
-        prompt.addAction(cameraAction)
-        prompt.addAction(photoLibraryAction)
-        prompt.addAction(photoAlbumAction)
-        
-        self.present(prompt, animated: true, completion: nil)
-    }
-
-
+           
+           // Create an Alert prompting user to select a option to pick an image
+           let prompt = UIAlertController(title: "Choose A Photo", message: "Please choose an option", preferredStyle: .alert)
+          
+           // Functions to provide in the handler for addAction to allow camera, photo album, photo libray usuage
+           
+           let imagePicker = UIImagePickerController()
+           imagePicker.delegate = self
+           
+           func presentCamera(_ _ : UIAlertAction) {
+               imagePicker.sourceType = .camera
+               present(imagePicker, animated: true, completion: nil)
+           }
+           
+           func presentPhotoLibrary(_ _ : UIAlertAction) {
+                      imagePicker.sourceType = .photoLibrary
+                      present(imagePicker, animated: true, completion: nil)
+                  }
+           
+           func presentPhotoAlbum(_ _ : UIAlertAction) {
+                             imagePicker.sourceType = .savedPhotosAlbum
+                             present(imagePicker, animated: true, completion: nil)
+                         }
+           
+           // Actions to add to the promt alert
+           
+           let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: presentCamera)
+           let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: presentPhotoLibrary)
+           let photoAlbumAction = UIAlertAction(title: "Photo Album", style: .default, handler: presentPhotoAlbum)
+           
+           // Add actions with Function handler to the prompt
+           
+           prompt.addAction(cameraAction)
+           prompt.addAction(photoLibraryAction)
+           prompt.addAction(photoAlbumAction)
+           
+           self.present(prompt, animated: true, completion: nil)
+       }
 }
